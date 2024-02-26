44a4c7ec337308f84d28ca781eee5e48c156d87c
Young-Sideways <young.sideways@mail.ru>
2024-02-26 16:20:43 +0300

    RU_ru translations update

--- Cinema_ru_RU.ts
+++ Cinema_ru_RU.ts
@@ -1,3 +1,134 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="Cinema_ru_RU"></TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AgeLimitation</name>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="8"/>
+        <source>Pegi 3</source>
+        <translation>Pegi 3</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="9"/>
+        <source>Pegi 7</source>
+        <translation>Pegi 7</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="10"/>
+        <source>Pegi 12</source>
+        <translation>Pegi 12</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="11"/>
+        <source>Pegi 16</source>
+        <translation>Pegi 16</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="12"/>
+        <source>Pegi 18</source>
+        <translation>Pegi 18</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/agelimitation.cpp" line="13"/>
+        <source>Pegi Special</source>
+        <translation>Pegi Special</translation>
+    </message>
+</context>
+<context>
+    <name>Character</name>
+    <message>
+        <location filename="app/character.h" line="12"/>
+        <source>No name</source>
+        <translation>Нет имени</translation>
+    </message>
+</context>
+<context>
+    <name>Film</name>
+    <message>
+        <location filename="app/Film/film.h" line="31"/>
+        <source>No limitations</source>
+        <translation>Нет ограничений</translation>
+    </message>
+    <message>
+        <location filename="app/Film/film.h" line="36"/>
+        <source>Limitations: %1</source>
+        <translation>Ограничения: %1</translation>
+    </message>
+    <message>
+        <location filename="app/Film/film.h" line="43"/>
+        <source>No genres</source>
+        <translation>Нет жанров</translation>
+    </message>
+    <message>
+        <location filename="app/Film/film.h" line="48"/>
+        <source>Genres: %1</source>
+        <translation>Жанры: %1</translation>
+    </message>
+</context>
+<context>
+    <name>Hall</name>
+    <message>
+        <location filename="app/Cinema/hall.cpp" line="19"/>
+        <location filename="app/Cinema/hall.cpp" line="29"/>
+        <source>Seat row out of hall rows range</source>
+        <translation>Ряд места выходит за границы мест в кинозале</translation>
+    </message>
+    <message>
+        <location filename="app/Cinema/hall.cpp" line="21"/>
+        <location filename="app/Cinema/hall.cpp" line="31"/>
+        <source>Seat place out of hall places range</source>
+        <translation>Номер места выходит за границы номеров в кинозале</translation>
+    </message>
+    <message>
+        <location filename="app/Cinema/hall.cpp" line="27"/>
+        <source>Seat cannot be null</source>
+        <translation>Место не может быть null</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>PhysiologicalLimitation</name>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="8"/>
+        <source>Change theme content</source>
+        <translation>Контент содержащий темы, переопределяющие традиционные ценнсоти</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="9"/>
+        <source>Sexual content</source>
+        <translation>Контент содержащий действия сексуального характера</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="10"/>
+        <source>Violence content</source>
+        <translation>Контент содержащий насилие</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="11"/>
+        <source>Bad language content</source>
+        <translation>Контент содержащий нецензурную лексику</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="12"/>
+        <source>Horror content</source>
+        <translation>Контент содержащий пугающие сцены</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="13"/>
+        <source>Drugs content</source>
+        <translation>Контент содержащий наркотики</translation>
+    </message>
+    <message>
+        <location filename="app/Film/Limitation/physiologicallimitation.cpp" line="14"/>
+        <source>Imitative behavior  content</source>
+        <translation>Контент содержащий внушение</translation>
+    </message>
+</context>
+</TS>
